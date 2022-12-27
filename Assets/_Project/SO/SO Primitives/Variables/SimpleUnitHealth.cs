9902d76bc6f5050085e6bd88169aae8847eae79a
Dante Iván García <61606117+VVizard22@users.noreply.github.com>
2022-12-26 22:57:36 -0300

    Initial commit

---
+++ Assets/_Project/SO/SO Primitives/Variables/SimpleUnitHealth.cs
@@ -0,0 +1,38 @@
+﻿// ----------------------------------------------------------------------------
+// Unite 2017 - Game Architecture with Scriptable Objects
+// 
+// Author: Ryan Hipple
+// Date:   10/04/17
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
+using RoboRyanTron.Unite2017.Events;
+
+namespace RoboRyanTron.Unite2017.Variables
+{
+    public class SimpleUnitHealth : MonoBehaviour
+    {
+        public FloatVariable HP;
+
+        public bool ResetHP;
+
+        public FloatReference StartingHP;
+        public GameEvent PlayerDamageEvent;
+
+        private void Start()
+        {
+            if (ResetHP)
+                HP.SetValue(StartingHP);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            DamageDealer damage = other.gameObject.GetComponent<DamageDealer>();
+            if (damage != null)
+            {
+                HP.ApplyChange(-damage.DamageAmount);
+                PlayerDamageEvent.Raise();
+            }
+        }
+    }
+}
